ee65f701bc33427482dc261247e0afa8c6810a09
Cameron Prebble <cameron.prebble@abletech.co.nz>
2015-09-29 15:16:28 +0200

    Initial commit

---
+++ scraper.rb
@@ -0,0 +1,4 @@
+nations = ["Australia", "England", "Fiji", "Uruguay", "Wales", "Japan", "Samoa",
+  "Scotland", "South Africa", "United States", "Argentina", "Georgia",
+  "Namibia", "New Zealand", "Tonga", "Canada", "France", "Ireland", "Italy",
+  "Romania"]
