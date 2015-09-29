407bc12b636c733ad4b41c6e27435e0313dcf46f
Cameron Prebble <cameron.prebble@abletech.co.nz>
2015-09-29 19:05:15 +0200

    add club nation and dob

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,20 @@
 require 'scraperwiki'
 
 class Player
+  LOCKED_NATIONS = ["New Zealand", "France", "Ireland"]
+
   def initialize(nation, row)
     @nation = nation
     @row = row
   end
 
+  def as_json
+    { name: name, link: link, nation: @nation, date_of_birth: date_of_birth,
+      club_nation: club_nation }
+  end
+
   def save!
-    ScraperWiki.save_sqlite(["name", "nation"], {"name" => name, "link" => link, "nation" => @nation})
+    ScraperWiki.save_sqlite([:name, :nation], as_json)
   end
 
   def name
@@ -16,5 +23,16 @@ class Player
 
   def link
     "https://en.wikipedia.org" + @row.css(".vcard a").first['href']
+  end
+
+  def club_nation
+    return @nation if LOCKED_NATIONS.include?(@nation)
+    return nil unless @row.css("span.flagicon a").length > 0
+    @row.css("span.flagicon a").first['title']
+  end
+
+  def date_of_birth
+    return nil unless @row.css("span.bday").length > 0
+    Date.parse(@row.css("span.bday").first.text)
   end
 end
